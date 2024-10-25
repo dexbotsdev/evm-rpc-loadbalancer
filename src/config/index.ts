7b334f5ca191e2a5a09f7446104866dfbbc25898
D <ddisdead462@gmail.com>
2024-10-25 16:12:17 +0500

    chore(fantom): added rpcs

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ import { BASE } from "./base";
 import { BSC } from "./bsc";
 import { OP } from "./op";
 import { POLYGON } from "./polygon";
+import { FANTOM } from "./fantom";
 
 export type TEnabledChain = typeof CHAINS_ENABLED[number];
 export type TConfig = Record<TEnabledChain, string[]>;
@@ -11,13 +12,15 @@ export const CHAINS_ENABLED = [
     "bsc",
     "base",
     "polygon",
-    "optimism"
+    "optimism",
+    "fantom"
 ] as const
 
 
 export const config: TConfig = {
-    bsc : BSC,
+    bsc: BSC,
     base: BASE,
     optimism: OP,
-    polygon : POLYGON
+    polygon: POLYGON,
+    fantom: FANTOM
 } 
